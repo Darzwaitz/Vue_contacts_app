869f2322d1efa50f54f3f12e99ecbe10b8c1ca21
darzwaitz <darzwaitz@yahoo.com>
2022-04-19 14:46:05 +0100

    Router routes and components to be used set up

--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: 'coaches' }, // redirect to this page when no page chosen
-    { path: '/coaches', component: null },
+    { path: '/coaches', CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: 'contact', component: null }],
+      component: CoachDetail,
+      children: [{ path: 'contact', component: ContactCoach }],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null }, // NB notFound can be any name
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound }, // NB notFound can be any name
   ],
 });
 

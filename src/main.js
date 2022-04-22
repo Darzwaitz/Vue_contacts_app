20df70cfafee542a1ab6d9a34ac09e7f40fe59ec
darzwaitz <darzwaitz@yahoo.com>
2022-04-22 14:00:33 +0100

    styles added to links with slots

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/index';
 import App from './App.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
 
 const app = createApp(App);
 
-app.use(router);
-app.use(store);
+app.use(router).use(store);
+
+app
+  .component('base-card', BaseCard)
+  .component('base-button', BaseButton)
+  .component('base-badge', BaseBadge);
 
 app.mount('#app');

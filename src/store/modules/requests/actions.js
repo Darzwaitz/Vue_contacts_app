576d520639a84470b8b1716327def29c7bf8ad8d
darzwaitz <darzwaitz@yahoo.com>
2022-05-12 15:09:09 +0100

    sending requestd to server database - TBU

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,23 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
       id: new Date().toISOString(),
       coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `https://vue-coach-app-7b128-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    newRequest.id = responseData.name;
+
     context.commit('addRequest', newRequest);
   },
 };

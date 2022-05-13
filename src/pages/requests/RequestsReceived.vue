b5ed0557b022e2283833012fcbb762cd2ec5b8d1
darzwaitz <darzwaitz@yahoo.com>
2022-05-13 17:14:57 +0100

    fetching requests from server - output to requests component - spinner added

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,17 @@
 <template>
+  <!-- teleported to body -->
+  <!-- string converted to actual boolean value with !!  -->
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
+  <!-- / teleport -->
   <section>
     <base-card>
       <header>
         <h2>Requestz Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
@@ -21,6 +28,12 @@
 import RequestItem from '../../components/requests/RequestItem.vue';
 
 export default {
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   components: {
     RequestItem,
   },
@@ -30,6 +43,23 @@ export default {
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };

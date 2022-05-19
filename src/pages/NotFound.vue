c56c1c6241a2781f0bee53bfba42268b780f5128
darzwaitz <darzwaitz@yahoo.com>
2022-05-19 20:42:03 +0100

    Not found page updated

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - check out all of our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
+<base-card></base-card>

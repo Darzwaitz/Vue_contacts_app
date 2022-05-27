0dcacf48c3252ca49e715f2ccceadac743ec685c
darzwaitz <darzwaitz@yahoo.com>
2022-05-27 17:12:53 +0100

    setting up auth - actions

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,24 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBeyO130rOG8qkI2ZzGJH2JxZh8bzGE5Xk',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'Failed to authenticate');
+      throw error;
+    }
+    console.log(responseData);
+  },
+};

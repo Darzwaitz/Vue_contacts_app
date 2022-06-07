fc05addb3b4b72a953d8043326847b29d588dbc6
darzwaitz <darzwaitz@yahoo.com>
2022-06-07 23:03:25 +0100

    refactoring to asynchronous components

--- src/router.js
+++ src/router.js
@@ -1,13 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory,
+  // defineAsyncComponent,
+} from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+// in clip, defined like this, but doesn't work - changed to the above
+// const CoachDetail = defineAsyncComponent(() =>
+//   import('./pages/coaches/CoachDetail.vue')
+// );
 
 const router = createRouter({
   history: createWebHistory(),
